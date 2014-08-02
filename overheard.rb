9417516a25903375f9f7809d115f71fb343e071d
Zee Spencer <zee@zeespencer.com>
2014-08-01 17:43:08 -0700

    Show overheards on the home page
    We've done three things:
    1. Defined an `Overheard`
       [model](https://github.com/codeunion/fundamentals-of-web-development/wiki/Glossary#model-mvc)
       for storing quotes in the database.
    2. Provided a list of all the `Overheard`s stored in the database to the
       views using the `@overheards` instance variable in the `get "/"`
       route
    3. Iterated over each of the `@overheards` and put them inside of a
       [`paragraph`
       tag](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/p)
    
    This allows our test to pass; since we can now store Overheards and are
    showing them on the screen.

--- overheard.rb
+++ overheard.rb
@@ -1,6 +1,29 @@
 require 'sinatra'
 require 'config/environment'
 
-get '/' do
+class Overheard
+  include DataMapper::Resource
+  # Declare our Overheard model is a DataMapper::Resource
 
+  property :id, Serial
+  property :body, Text
+  property :created_at, DateTime
+
+  # Properties what data our Overheard model contains. The first argument is
+  # the name we use to reference the data, while the second is the type of data.
+  #
+  # http://datamapper.org/docs/properties.html
+end
+
+DataMapper.finalize
+# Declares we're done defining our models.
+# http://rdoc.info/github/datamapper/dm-core/DataMapper#finalize-class_method
+
+DataMapper.auto_upgrade!
+# Upgrades the database schema to reflect our models.
+# http://rubydoc.info/github/datamapper/dm-migrations/DataMapper/Migrations/Repository#auto_upgrade%21-instance_method
+
+get '/' do
+  @overheards = Overheard.all
+  erb :home
 end
